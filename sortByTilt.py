d407f93718ae90ec62955847084c0c25eca4e58e
feisenstein <fabian.eisenstein@biol.ethz.ch>
2019-10-10 17:44:57 +0200

    Add files via upload

--- sortByTilt.py
+++ sortByTilt.py
@@ -6,7 +6,7 @@
 # Called From:  User, makeAlignedStack
 # Author:       Fabian Eisenstein
 # Revision:     v1.2 
-# Last Change:	2019/10/10: optimizations
+# Last Change:	2019/10/10: create sorted dose file for IMOD, optimizations
 #				2019/03/06: implemented reading of .tlt-like files
 #				2018/03/13: minor text fixes
 # Created:		2018/03/12
@@ -21,17 +21,26 @@ parser = argparse.ArgumentParser(description='Reads .mdoc file or .tlt file, sor
 parser.add_argument('inputStack',help='Path to input stack.')
 parser.add_argument('outputStack',help='Path to output stack.')
 parser.add_argument('tiltFile',help='Path to .mdoc or .tlt file containing tilt angles.')
+parser.add_argument('--dfile',dest='dfile',action='store_true',default=False,help='Create dose file for IMOD. Requires exposure dose set in .mdoc file or --dose input!')
+parser.add_argument('--dose',dest='dose',type=float,default=0,help='Dose per tilt to write dose file for IMOD. Is ignored unless --dfile is set.')
 
 args = parser.parse_args()
 instack = args.inputStack
 outstack = args.outputStack
 mdocFileName = args.tiltFile
 
+dfile = args.dfile
+dose = args.dose
+
+if dose > 0 and not dfile:
+	print ("--dfile is not set. Dose value is ignored.")
+
 
 def readMdoc(raw):                                                   		#Makes list of Mics for sorting
 	raw.seek(0)
 	mics = []
 	mic = []
+	totalDose = 0
 	for line in raw:
 		col = line.split(" ")
 		if col[0] == "[ZValue":
@@ -40,8 +49,20 @@ def readMdoc(raw):                                                   		#Makes li
                 	
 		if col[0] == "TiltAngle":
 			mic.append(float(col[2]))
+			if dfile and dose > 0:
+				mic.append(totalDose)
+				mic.append(dose)
+				totalDose += dose
+			if not dfile or (dfile and dose > 0):
+				mics.append(mic)
+				mic = []
+		if dfile and dose == 0 and col[0] == "ExposureDose":
+			mic.append(totalDose)
+			mic.append(float(col[2]))
+			totalDose += float(col[2])
 			mics.append(mic)
-			mic = []	
+			mic = []
+
 	return mics
 	
 def readTilt(raw):                                                   		#Makes list of Mics for sorting
@@ -49,10 +70,17 @@ def readTilt(raw):                                                   		#Makes li
 	mics = []
 	mic = []
 	zval = 0
+	totalDose = 0
 	for line in raw:
 		col = line.strip()
 		mic.append(str(zval))
 		mic.append(float(col))
+
+		if dfile and dose > 0:
+			mic.append(totalDose)
+			mic.append(dose)
+			totalDose += dose
+
 		mics.append(mic)
 		mic = []
 		zval += 1
@@ -78,7 +106,7 @@ if os.path.exists(mdocFileName):                                                
 
 	if os.path.exists(instack):
 
-		command = "newstack -in " + instack + " -ou " + outstack + " "
+		command = "newstack -in " + instack + " -ou " + outstack + " -quiet "
 		if mdoc == 1: 
 			command = command + "-mdoc " 
 		command = command + "-se "
@@ -87,14 +115,30 @@ if os.path.exists(mdocFileName):                                                
 		command = command.strip(",")
 
 		print(command)
-		if instack.split('.')[0] != mdocFileName.split('.')[0]:
+		if mdoc == 1 and instack.split('.')[0] != mdocFileName.split('.')[0]:
 			os.system("cp " + mdocFileName + " " + instack + ".mdoc")
 
 		os.system(command)
-		if instack != outstack and instack.split('.')[0] != mdocFileName.split('.')[0]:
+		if mdoc == 1 and instack != outstack and instack.split('.')[0] != mdocFileName.split('.')[0]:
 			os.system("rm " + instack + ".mdoc")
 		if mdoc ==1:
 			print ("New mdoc file was written!")
+
+		if dfile:
+			doseoutput = ""
+			for mic in newmdocList:
+				if len(mic) > 3:
+					doseoutput = doseoutput + str(mic[2]) + "\t" + str(mic[3]) + "\n"
+				else:
+					print ("Dose value was not found! No dose file written!")
+					break
+			if doseoutput != "":
+				writeDose = open(outstack.split(".")[0] + "_dose.txt","w+")
+				writeDose.seek(0,0)
+				writeDose.write(doseoutput)
+				writeDose.close()
+				print ("Dose file was written!")
+
 	else:
 		print (instack + " doesn't exist!\n")
 else:
